e628da6496f8f93ccc059fc6da86d2dba5771af7
ruanyl <ruanyu1@gmail.com>
2016-11-04 22:12:43 +0200

    Add exponential moving average

--- src/movingAverage.js
+++ src/movingAverage.js
@@ -1,6 +1,17 @@
+import { last } from 'ramda'
 import { preSum } from './arrayMath'
 
 export const sma = (n = 5, f) => (data = []) => {
   const finalData = f === undefined ? data : data.map(f)
   return finalData.reduce((pv, cv, idx) => (idx < n - 1 ? pv.concat([null]) : pv.concat([preSum(n, idx, finalData) / n])), [])
+}
+
+const emaFormula = (n, today, yesterday) => {
+  const k = 2 / (n + 1)
+  return (k * today) + (yesterday * (1 - k))
+}
+
+export const ema = (n = 5, f) => (data = []) => {
+  const finalData = f === undefined ? data : data.map(f)
+  return finalData.reduce((pv, cv) => (last(pv) === undefined ? pv.concat(cv) : pv.concat(emaFormula(n, cv, last(pv)))), [])
 }
--- test/movingAverageSpec.js
+++ test/movingAverageSpec.js
@@ -1,6 +1,6 @@
 import test from 'ava'
 import { range } from 'ramda'
-import { sma } from '../src/movingAverage'
+import { sma, ema } from '../src/movingAverage'
 
 test('Simple Moving Avarage', t => {
   const arr = range(1, 20)
@@ -17,4 +17,13 @@ test('Simple Moving Avarage with selector', t => {
   const expectResults = [null, null, null, null, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
   t.deepEqual(fiveDaysSMA(arr), expectResults)
+})
+
+test('Exponential Moving Avarage', t => {
+  const arr = [22.81, 23.09, 22.91, 23.23, 22.83, 23.05, 23.02, 23.29, 23.41, 23.49, 24.60]
+  const nineDaysEMA = ema(9)
+  const expectResults = [22.81, 22.87, 22.87, 22.95, 22.92, 22.95, 22.96, 23.03, 23.10, 23.18, 23.47]
+  const returnedResults = nineDaysEMA(arr).map(v => Math.round(100 * v) / 100)
+
+  t.deepEqual(returnedResults, expectResults)
 })
