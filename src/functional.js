28d2b5ba564bfc341eb00b9feaa01b2e989051e5
ruanyl <ruanyu1@gmail.com>
2016-11-26 00:38:24 +0200

    refactor kdj

--- src/functional.js
+++ src/functional.js
@@ -1,4 +1,4 @@
-import { T, compose, reverse, last, cond, equals, transpose } from 'ramda'
+import { T, compose, reverse, last, cond, equals, transpose, head, juxt, drop } from 'ramda'
 
 /**
  * funcs = [func1, func2, func3]
@@ -22,11 +22,22 @@ export const mapCompose = funcs => {
 }
 
 export const mix = (f, ...args) => args[0].map((v, i) => f(...args.map(a => a[i])))
-export const mixAll = (fns, ...args) =>
-  transpose(
-    args[0].map(
-      (v, i) => fns.map(
-        fn => fn(...args.map(a => a[i]))
-      )
+
+export const mixAll = (fns, ...args) => transpose(
+  args[0].map(
+    (v, i) => fns.map(
+      fn => fn(...args.map(a => a[i]))
     )
   )
+)
+
+/**
+ * funcs: [f1, f2, f3, f4]
+ * return [f1(v), f2(v), f3(v), f4([f1(v), f2(v), f3(v)])]
+ */
+export const mapMix = funcs => d => {
+  const mixFn = head(funcs)
+  const mapFns = drop(1, funcs)
+  const mapL = juxt(mapFns)(d)
+  return [...mapL, mixFn(...mapL)]
+}
--- src/kdj.js
+++ src/kdj.js
@@ -1,16 +1,16 @@
-import { compose, nth } from 'ramda'
+import { compose, nth, curry } from 'ramda'
 import { hhv, llv, sma, close } from './movingAverage'
 import { LOW, HIGH } from './cons'
-import { mapCompose } from './functional'
+import { mapMix } from './functional'
 
-export const rsv = n => data => data.map((record, idx) => {
+export const rsv = curry(n => data => data.map((record, idx) => {
   const closePrice = close(idx, data)
   const highPrice = hhv(HIGH, n, idx)(data)
   const lowPrice = llv(LOW, n, idx)(data)
   return highPrice === lowPrice ? 0 : 100 * ((closePrice - lowPrice) / (highPrice - lowPrice))
-})
+}))
 
-export const k = n => rsvN => compose(sma(n), rsvN)
-export const d = n => kN => sma(n, kN)
+export const k = curry((kn, rn) => compose(sma(kn), rsv(rn)))
+export const d = curry((dn, kn, rn) => compose(sma(dn), k(kn, rn)))
 export const j = (kN, dN) => kN.map((kv, idx) => (3 * kv) - (2 * nth(idx, dN)))
-export const kdj = (rPeriods, kPeriods, dPeriods) => mapCompose([k(kPeriods)(rsv(rPeriods)), d(dPeriods), j])
+export const kdj = (dn, kn, rn) => mapMix([j, k(kn, rn), d(dn, kn, rn)])
