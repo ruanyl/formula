f64b71b8573595e704218b8495958fe98ea44959
ruanyl <ruanyu1@gmail.com>
2016-11-29 01:17:20 +0200

    added adx

---
+++ src/adx.js
@@ -0,0 +1,30 @@
+import { ema } from './movingAverage'
+import { atr } from './atr'
+import { mix } from './functional'
+import { HIGH, LOW } from './cons'
+
+const dmPlus = data => data.map((today, i) => {
+  if (i === 0) return 0
+  const yesterday = data[i - 1]
+  return (HIGH(today) - HIGH(yesterday)) > (LOW(yesterday) - LOW(today)) ? Math.max(HIGH(today) - HIGH(yesterday), 0) : 0
+})
+
+const dmMinus = data => data.map((today, i) => {
+  if (i === 0) return 0
+  const yesterday = data[i - 1]
+  return (LOW(yesterday) - LOW(today)) > (HIGH(today) - HIGH(yesterday)) ? Math.max(LOW(yesterday) - LOW(today), 0) : 0
+})
+
+const _divide = (a, b) => (b === 0 ? 0 : a / b)
+
+// const sumPeriods = (periods, data) => data.map((t, i) => (i < periods ? 0 : sum(periods, i, data)))
+
+/**
+ * https://www.tradingview.com/wiki/Directional_Movement_(DMI)#CALCULATION
+ */
+export const adx = periods => data => {
+  const atrD = ema(periods, atr(periods)(data))
+  const diPlus = ema(periods, mix(_divide, dmPlus(data), atrD)).map(m => 100 * m)
+  const diMinus = ema(periods, mix(_divide, dmMinus(data), atrD)).map(m => 100 * m)
+  return ema(periods, diPlus.map((p, i) => Math.abs(_divide((diPlus[i] - diMinus[i]), (diPlus[i] + diMinus[i]))) * 100))
+}
