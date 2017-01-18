e06ff2902ec041e04d44de786615f6ba4aeca3a4
ruanyl <ruanyu1@gmail.com>
2017-01-19 00:20:20 +0200

    Chaikin Money Flow

--- src/ad.js
+++ src/ad.js
@@ -1,15 +1,7 @@
 import { isNil, last, subtract, compose } from 'ramda'
-import { VOLUME, HIGH, LOW, CLOSE } from './cons'
 import { ema } from './movingAverage'
 import { mix } from './functional'
-import { sum } from './arrayMath'
-
-// Money Flow Multiplier
-// [(close - low) - (high - close)] / (high - low)
-const mfm = data => (HIGH(data) - LOW(data) === 0 ? 0 : ((CLOSE(data) - LOW(data)) - (HIGH(data) - CLOSE(data))) / (HIGH(data) - LOW(data)))
-
-// Money Flow Volume
-const mfv = data => mfm(data) * VOLUME(data)
+import { mfv } from './cmf'
 
 /**
  * Accumulation Distribution Line
@@ -28,11 +20,3 @@ export const adosc = (shortPeriods = 3, longPeriods = 10) => data => {
   return mix(compose(Math.round, subtract), ema(shortPeriods, adList), ema(longPeriods, adList))
 }
 
-/**
- * Chaikin Money Flow
- */
-export const cmf = periods => data => {
-  const mfvs = data.map(mfv)
-  const volumes = data.map(VOLUME)
-  return mfvs.map((m, i) => (i < periods - 1 ? 0 : sum(periods, i, mfvs) / sum(periods, i, volumes)))
-}
